6af89a01fce1288ee1b93b8c99428e1edfe9e987
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2022-12-15 17:24:09 +0545

    hello

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.use(helmet());
 
 app.use('/api', limiter);
 
-// if the file is greater then 10kb then i will not be accepted
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 
--- server.js
+++ server.js
@@ -1,1 +1,6 @@
 const app = require('./app');
+const port = 80;
+
+app.listen(80, () => {
+    console.log('SERVER IS RUNNING');
+})
