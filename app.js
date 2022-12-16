cd8f37c93205ecb3a8ade1f315525ea15e98911f
maheshbasnet089 <basnetmanish089@gmail.com>
2022-12-16 06:02:07 +0545

    add

--- app.js
+++ app.js
@@ -13,11 +13,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "./public")));
+// Load the TensorFlow.js library
+const tf = require("@tensorflow/tfjs");
+// Create a new TensorFlow.js model
+const model = async () => {
+  await tf.loadLayersModel("./model.json");
+};
+console.log(model);
+
+// Load the image data from a file or URL
+// const image = tf.node.decodeImage(fs.readFileSync("./public/images/0.png"), 3);
+// const image = Image();
+// image.src = "./public/images/0.png";
+const image = "../cat.jpg";
+
+// Make and format the predicitions
+const predictions = model.predict(image);
+const index = predictions.as1D().argMax();
+const classPrediction = index.dataSync()[0];
+
+// Print the predictions
+console.log("Result:", classPrediction);
 
 app.use("/", homeRoute);
 
 app.all("*", (req, res, next) => {
-  return res.send("Page not found");
+  return res.render("404");
 });
 
 // if the file is greater then 10kb then i will not be accepted
--- public/js/index.js
+++ public/js/index.js
@@ -1,53 +1,118 @@
-const speak = document.querySelector(".button");
-const text = document.querySelector(".textbox");
+// const speak = document.querySelector(".button");
+// const text = document.querySelector(".textbox");
 
-let speakOnClick = (message) => {
-  var msg = new SpeechSynthesisUtterance();
-  msg.text = "IF YOU ARE BLIND PRESS THE SCREEN THREE TIMES";
-  window.speechSynthesis.speak(msg);
-};
+// let speakOnClick = (message) => {
+//   var msg = new SpeechSynthesisUtterance();
+//   msg.text = "IF YOU ARE BLIND PRESS THE SCREEN THREE TIMES";
+//   window.speechSynthesis.speak(msg);
+// };
 
-// window.addEventListener("load", function () {
-//   // Call your function here
+// // window.addEventListener("load", function () {
+// //   // Call your function here
+// //   speakOnClick();
+// // });
+
+// window.onbeforeunload = function () {
 //   speakOnClick();
+// };
+// // speak.addEventListener("click", (el) => {
+// //   speakOnClick(text.value);
+// // });
+
+// // Check if the browser supports the Web Speech API
+
+// // Add a startRecognition function to the onclick event of a button
+// speak.onclick = function () {
+//   startRecognition();
+// };
+
+// function startRecognition() {
+//   if ("webkitSpeechRecognition" in window) {
+//     var recognition = new webkitSpeechRecognition();
+
+//     recognition.lang = "en-US";
+
+//     recognition.start();
+
+//     recognition.onstart = function () {
+//       console.log("Speech recognition has started.");
+//     };
+
+//     recognition.onend = function () {
+//       console.log("Speech recognition has ended.");
+//     };
+
+//     recognition.onresult = function (event) {
+//       var text = event.results[0][0].transcript;
+
+//       console.log("Recognized Text: " + text);
+//     };
+//   }
+// }
+
+// // CLICK TO OPEN CAMERA
+
+// const model = async () => {
+//   return await mobilenet.load();
+// };
+// const video = document.getElementById("video");
+
+// video.addEventListener("play", () => {
+//   const predictions = model.classify(video);
+//   console.log(predictions);
 // });
 
-window.onbeforeunload = function () {
-  speakOnClick();
-};
-// speak.addEventListener("click", (el) => {
-//   speakOnClick(text.value);
+// let camera_button = document.querySelector("#start-camera");
+// let video = document.querySelector("#video");
+// let click_button = document.querySelector("#click-photo");
+// let canvas = document.querySelector("#canvas");
+
+// camera_button.addEventListener("click", async function () {
+//   let stream = await navigator.mediaDevices.getUserMedia({
+//     video: true,
+//     audio: false,
+//   });
+//   video.srcObject = stream;
 // });
 
-// Check if the browser supports the Web Speech API
+// click_button.addEventListener("click", function () {
+//   canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+//   let image_data_url = canvas.toDataURL("image/jpeg");
 
-// Add a startRecognition function to the onclick event of a button
-speak.onclick = function () {
-  startRecognition();
+//   // data url of the image
+//   console.log(image_data_url);
+// });
+
+// Load the TensorFlow.js library
+// const tf = require("@tensorflow/tfjs");
+import * as tf from "@tensorflow/tfjs";
+
+// Create a new TensorFlow.js model
+const model = async () => {
+  await tf.loadLayersModel("./model.json");
 };
 
-function startRecognition() {
-  if ("webkitSpeechRecognition" in window) {
-    var recognition = new webkitSpeechRecognition();
+// Access the camera on the user's device
+const stream = async () => {
+  await navigator.mediaDevices.getUserMedia({
+    video: true,
+  });
+};
 
-    recognition.lang = "en-US";
+// Create a video element to display the camera feed
+const video = document.createElement("video");
+video.srcObject = stream;
+video.play();
 
-    recognition.start();
+// Set up a loop to process each video frame
+setInterval(async () => {
+  // Capture a frame from the video
+  const frame = tf.browser.fromPixels(video);
 
-    recognition.onstart = function () {
-      console.log("Speech recognition has started.");
-    };
+  // Pass the frame to the model to make predictions
+  const predictions = await model.predict(frame);
+  console.log(predictions);
 
-    recognition.onend = function () {
-      console.log("Speech recognition has ended.");
-    };
-
-    recognition.onresult = function (event) {
-      var text = event.results[0][0].transcript;
-
-      console.log("Recognized Text: " + text);
-    };
-  }
-}
-
-// CLICK TO OPEN CAMERA
+  // Use the predictions to update the page
+  // ...
+}, 1000 / 30);
