e15fe6b255360661439c41855e674cf9efdce210
maheshbasnet089 <basnetmanish089@gmail.com>
2022-12-18 21:20:53 +0545

    addd

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+const port = 3000 || 5000;
 const app = require("./app");
 const mongoConnection = require("./database/db");
 require("dotenv").config();
 
 mongoConnection(process.env.MONGO_URI);
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("server has started on port 3000");
 });
