c3ad49f4af85f8b90a9fadc1ec4bd6d55ed30930
Abijeet <63050833+Abijeet-Ind@users.noreply.github.com>
2022-12-19 10:32:44 +0545

    Update homeRoute.js
    Updates route

--- routes/homeRoute.js
+++ routes/homeRoute.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { homeController, test } = require("../controllers/homeController");
+const { homeController, test, imageDB } = require("../controllers/homeController");
 
 const router = express.Router();
 
 router.route("/home").get(homeController);
 router.route("/test").get(test);
-
+router.route("/test").post(test);
 module.exports = router;
